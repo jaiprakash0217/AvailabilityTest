ba0876369f380f0df004c4c99add1e2c81201ddb
Jai Prakash <73898052+jaiprakash0217@users.noreply.github.com>
2024-04-25 00:04:38 +0530

    Update AvailabilityTestDemo.cs

--- src/AvailabilityTestDemoFunctionApp/AvailabilityTestDemo.cs
+++ src/AvailabilityTestDemoFunctionApp/AvailabilityTestDemo.cs
@@ -1,1 +1,21 @@
 
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Contoso.AvailabilityTest.Startup))]
+
+
+namespace Contoso.AvailabilityTest
+{
+    /// <summary>
+    /// Startup class to set up dependencies for the Azure Function
+    /// </summary>
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+
+        }
+    }
+}
