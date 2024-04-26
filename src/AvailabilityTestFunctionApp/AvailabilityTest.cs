8fb77c1bb9532c01fef6d41715ff00136e4f7e55
Jai Prakash <73898052+jaiprakash0217@users.noreply.github.com>
2024-04-26 18:20:15 +0530

    Update AvailabilityTest.cs

--- src/AvailabilityTestFunctionApp/AvailabilityTest.cs
+++ src/AvailabilityTestFunctionApp/AvailabilityTest.cs
@@ -49,7 +49,7 @@ namespace KPIReporting.AvailabilityTest
             {
                 // Make a request to the test app that we monitor for availability
                  _httpClient.BaseAddress = new Uri("https://usawu2gdpcntrl-dev-wap.usawu2gdpcntrl-dev-ase.appserviceenvironment.net");
-                using HttpResponseMessage response = await _httpClient.GetAsync(_testAppUrl);
+                using HttpResponseMessage response = await _httpClient.GetAsync("/");
                 // Ensure we get a successful response (typically 200 OK). Otherwise, an exception will be thrown
                 response.EnsureSuccessStatusCode();
                 log.LogInformation($"Successful response! Response code for {_testAppUrl}: {response.StatusCode} ");
