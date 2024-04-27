dfed6b7789153fd77c9609a3502b9a3aa357f4bb
Jai Prakash <73898052+jaiprakash0217@users.noreply.github.com>
2024-04-27 13:05:51 +0530

    Update AvailabilityTest.cs

--- src/AvailabilityTestFunctionApp/AvailabilityTest.cs
+++ src/AvailabilityTestFunctionApp/AvailabilityTest.cs
@@ -18,6 +18,8 @@ namespace KPIReporting.AvailabilityTest
     {
         private readonly TelemetryClient _telemetryClient;
         private readonly HttpClient _httpClient;
+        private readonly string _userName;
+        private readonly string _password;
         private readonly string _testAppUrl;
         private readonly string _testJob1Url;
         private readonly string _testJob2Url;
@@ -30,6 +32,8 @@ namespace KPIReporting.AvailabilityTest
         {
             _telemetryClient = telemetryClient;
             _httpClient = httpClientFactory.CreateClient();
+            _userName = configuration["WebUser"];
+            _password = configuration["Password"];
             _testAppUrl = configuration["TestAppUrl"];
             _testJob1Url = configuration["TestJob1Url"];
             _testJob2Url = configuration["TestJob2Url"];
@@ -48,31 +52,35 @@ namespace KPIReporting.AvailabilityTest
             try
             {
                 // Make a request to the test app that we monitor for availability
-                 //_httpClient.BaseAddress = new Uri("https://usawu2gdpcntrl-dev-wap.scm.usawu2gdpcntrl-dev-ase.appserviceenvironment.net");
-                //_httpClient.DefaultRequestHeaders.Add($"Authorization", $"Basic dXNhd3UyZ2RwY250cmwtZGV2LXdhcDowRE00YVN2d1dXTTltSjI4R01xR3JjRlF0YUtQQjdiMTM1WnE0dmc0cEpGR2pUNjFudEdmRFQyNVRYU1g=");
-                _httpClient.DefaultRequestHeaders.Add($"Authorization", $"Basic JHVzYXd1MmdkcGNudHJsLWRldi13YXA6MERNNGFTdndXV005bUoyOEdNcUdyY0ZRdGFLUEI3YjEzNVpxNHZnNHBKRkdqVDYxbnRHZkRUMjVUWFNY");
-                using HttpResponseMessage response = await _httpClient.GetAsync("https://usawu2gdpcntrl-dev-wap.scm.usawu2gdpcntrl-dev-ase.appserviceenvironment.net/api/continuouswebjobs/MetallurgyReportWebJob");
+                //_httpClient.DefaultRequestHeaders.Add($"Authorization", $"Basic JHVzYXd1MmdkcGNudHJsLWRldi13YXA6MERNNGFTdndXV005bUoyOEdNcUdyY0ZRdGFLUEI3YjEzNVpxNHZnNHBKRkdqVDYxbnRHZkRUMjVUWFNY");
+                var byteArray = Encoding.ASCII.GetBytes("{_userName}:{_password}");
+                _httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+                using HttpResponseMessage response = await _httpClient.GetAsync(_testAppUrl);
                 // Ensure we get a successful response (typically 200 OK). Otherwise, an exception will be thrown
                 var JobStatus = await response.Content.ReadAsStringAsync();  
                 response.EnsureSuccessStatusCode();
-                log.LogInformation($"Successful response! Response code for base URL: {response} : {JobStatus} ");
+                log.LogInformation($"Successful response! Response code for Base URL: {response} : {JobStatus.status} ");
                
 
                 // Repeat this task for all web jobs
                 // Make a request to the test app that we monitor for availability
-                using HttpResponseMessage response1 = await _httpClient.GetAsync("https://usawu2gdpcntrl-dev-wap.scm.usawu2gdpcntrl-dev-ase.appserviceenvironment.net/api/continuouswebjobs/MetallurgyReportWebJob1");
+                using HttpResponseMessage response1 = await _httpClient.GetAsync(_testJob1Url);
                 
                 // Ensure we get a successful response (typically 200 OK). Otherwise, an exception will be thrown
-                response1.EnsureSuccessStatusCode();
-                log.LogInformation($"Successful response! Response code for MetallurgyReportWebJob1: {response1} ");
+                var Job1Status = await response1.Content.ReadAsStringAsync();  
+                log.LogInformation($"Successful response! Response code for _testJob1Url: {Job1Status.status} ");
+                if !(Job1Status.status == "Running" || Job1Status.status == "Completed")
+                    throw new ArgumentException("Please start web job.");
                 
-              /*  
                  // Make a request to the test app that we monitor for availability
                 using HttpResponseMessage response2 = await _httpClient.GetAsync(_testJob2Url);                
                 // Ensure we get a successful response (typically 200 OK). Otherwise, an exception will be thrown
+                var Job2Status = await response2.Content.ReadAsStringAsync();
                 response2.EnsureSuccessStatusCode();
-                log.LogInformation($"Successful response! Response code for {_testJob2Url}: {response2.StatusCode} ");
-
+                log.LogInformation($"Successful response! Response code for {_testJob2Url}: {Job2Status.status}");
+                 if !(Job2Status.status == "Running" || Job2Status.status == "Completed")
+                    throw new ArgumentException("Please start web job.");
+/*
                  // Make a request to the test app that we monitor for availability
                 using HttpResponseMessage response3 = await _httpClient.GetAsync(_testJob3Url);                
                 // Ensure we get a successful response (typically 200 OK). Otherwise, an exception will be thrown
