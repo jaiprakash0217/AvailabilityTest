7862f69385a9d2152371701ec2f632fe006bbcdc
Jai Prakash <73898052+jaiprakash0217@users.noreply.github.com>
2024-04-27 14:09:37 +0530

    Update AvailabilityTest.cs

--- src/AvailabilityTestFunctionApp/AvailabilityTest.cs
+++ src/AvailabilityTestFunctionApp/AvailabilityTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Text;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Script.Serialization;
 using Microsoft.ApplicationInsights;
 using Microsoft.ApplicationInsights.DataContracts;
